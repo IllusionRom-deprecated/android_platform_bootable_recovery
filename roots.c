CyanogenMod/android_bootable_recovery

Fix mkfs.f2fs calling in roots
Change-Id: I4991732be70a7e9ae7e87fa1395e89b315717049

roots.c
@@ -410,9 +410,10 @@ int format_volume(const char* volume) {
 
 #ifdef USE_F2FS
     if (strcmp(v->fs_type, "f2fs") == 0) {
-        int result = make_f2fs_main(v->blk_device, v->mount_point);
+        const char* args[] = { "mkfs.f2fs", v->device };
+        int result = make_f2fs_main(2, (char**)args);
         if (result != 0) {
-            LOGE("format_volume: mkfs.f2f2 failed on %s\n", v->blk_device);
+            LOGE("format_volume: mkfs.f2fs failed on %s\n", v->blk_device);
             return -1;
         }
         return 0;

IllusionRom-deprecated/android_platform_bootable_recovery

roots.c
@@ -423,9 +423,10 @@ int format_volume(const char* volume) {
 
 #ifdef USE_F2FS
     if (strcmp(v->fs_type, "f2fs") == 0) {
-        int result = make_f2fs_main(v->blk_device, v->mount_point);
+        const char* args[] = { "mkfs.f2fs", v->blk_device };
+        int result = make_f2fs_main(2, (char**)args);
         if (result != 0) {
-            LOGE("format_volume: mkfs.f2f2 failed on %s\n", v->blk_device);
+            LOGE("format_volume: mkfs.f2fs failed on %s\n", v->blk_device);
             return -1;
         }
         return 0;